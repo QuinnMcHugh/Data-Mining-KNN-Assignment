59e6e2a216c70b81f2d6d69ba66b6665a790eb79
QuinnMcHugh <quinnmchugh.us@gmail.com>
2017-02-20 11:46:27 -0500

    cleaned up code, tested my own data as example

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,15 +3,34 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        CSVData csvIris = new CSVData("input-data/Iris_Test.csv");
+        CSVData csvIncome = new CSVData("input-data/Income_Test.csv");
 
-        DataFrame dfIris = new IrisDataFrame();
-        dfIris.parseFromCSV(csvIris);
+        DataFrame dfIncome = new IncomeDataFrame();
+        dfIncome.parseFromCSV(csvIncome);
 
         SimilarityMetric metric = new SimilarityMetric(SimilarityMetric.MetricType.EUCLIDEAN);
-        DataRecord compare = dfIris.getRecords().get(21);
         int k = 12;
+        // training set
+        // test set
 
-        ProximityCalculator.produceTable(dfIris, dfIris, k, metric, "out/Iris_Output.csv");
+        DataFrame dfIncomeTest = new IncomeDataFrame();
+        IncomeRecord me = new IncomeRecord();
+        me.age = 22;
+        me.capitalActivity = 0;
+        me.educationCat = 14;
+        me.education = "Masters";
+        me.gender = "Female";
+        me.hoursPerWeek = 40;
+        me.incomeClass = ">50k";
+        me.maritalStatus = "Never-married";
+        me.nativeCountry = "United-States";
+        me.occupation = "Prof-specialty";
+        me.race = "White";
+        me.relationship = "Not-in-family";
+        me.workclass = "Local-gov";
+
+        dfIncomeTest.getRecords().add(me);
+
+        ProximityCalculator.produceTable(dfIncome, dfIncomeTest, k, metric, "out/Income_Output.csv");
     }
 }
--- src/com/company/ProximityCalculator.java
+++ src/com/company/ProximityCalculator.java
@@ -19,7 +19,7 @@ public class ProximityCalculator {
 
 			// Compute score between records[i] and compare
 			siMapping.score = metric.calculate(compare.toVector(), records.get(i).toVector());
-			siMapping.index = records.get(i).getID();
+			siMapping.index = i;
 
 			// Add new ScoreIndex pair to heap
 			heap.add(siMapping);
@@ -47,7 +47,7 @@ public class ProximityCalculator {
 
 		// Compile Map of each of the k nearest neighbors by class label to count in k-nearest
 		for (ScoreIndexMapping siMapping : kMostSimilar){
-			String type = records.get(siMapping.index - 1).getClassName();
+			String type = records.get(siMapping.index).getClassName();
 			if (classOccurrences.containsKey(type)){
 				classOccurrences.put(type, classOccurrences.get(type) + 1);
 			}
@@ -119,109 +119,4 @@ public class ProximityCalculator {
 			e.printStackTrace();;
 		}
 	}
-
-//	public static List<Double> getKMostSimilar(DataFrame input, DataRecord compare, int k, String metric){
-//		// this method should return one row in the final output table
-//
-//		// go through each record in dataframe and compute similarity score
-//		// then go back and find k best scores.
-//		// extract ID and score information of these and return
-//
-//		List<Double> scores = new ArrayList<Double>();
-//		List<DataRecord> records = input.getRecords();
-//
-//		for (int i = 0; i < records.size(); i++){
-//			List<Double> vector = records.get(i).toVector();
-//			double result = 0d;
-//
-//			// compute the distance between the two vectors
-//			if (metric.equals("euclidean")){
-//				result = SimilarityMetrics.euclideanDistance(vector, compare.toVector());
-//			}
-//			else if (metric.equals("cosine")){
-//				result = SimilarityMetrics.cosineSimilarity(vector, compare.toVector());
-//			}
-//
-//			// add result to scores listing
-//			scores.add(result);
-//		}
-//
-//		List<Double> topKScores = new ArrayList<Double>();
-//		List<Integer> topKIDs = new ArrayList<Integer>();
-//
-//		for (int i = 0; i < k; i++){
-//			// find min value in scores
-//			int minIndex = getMinIndex(scores);
-//			topKIDs.add(records.get(minIndex).getID());
-//			topKScores.add(scores.get(minIndex));
-//
-//			// set previously found value to large number and find next smallest
-//			scores.set(minIndex, Double.MAX_VALUE);
-//		}
-//
-//		// weave together top k ID's and scores into one list formatted for table
-//		List<Double> ID_proximity = new ArrayList<Double>();
-//		for (int i = 0; i < topKIDs.size(); i++){
-//			ID_proximity.add((double)topKIDs.get(i));
-//			ID_proximity.add(topKScores.get(i));
-//		}
-//
-//		return ID_proximity;
-//	}
-//
-//	// returns the index with the smallest value
-//	private static int getMinIndex(List<Double> scores){
-//		double min = Double.MAX_VALUE;
-//		int minIndex = 0;
-//
-//		for (int i = 0; i < scores.size(); i++){
-//			if (scores.get(i) < min){
-//				min = scores.get(i);
-//				minIndex = i;
-//			}
-//		}
-//
-//		return minIndex;
-//	}
-//
-//	public static void produceTable(DataFrame input, DataFrame test, int k, String metric, String outputFileName){
-//		// Set up table headers
-//		String fileText = "Trans. ID,";
-//		for (int i = 1; i <= k; i++){
-//			fileText += (i + " ID,");
-//			fileText += (i + " Prox.,");
-//		}
-//		fileText += "\n";
-//
-//		List<DataRecord> testRecords = test.getRecords();
-//
-//		// iterate through each test record, compute k best matches, add to file text
-//		for (int i = 0; i < testRecords.size(); i++){
-//			List<Double> similar = getKMostSimilar(input, testRecords.get(i), k, metric);
-//
-//			fileText += ((i + 1) + ",");
-//			for (int j = 0; j < similar.size(); j++){
-//				if (j % 2 == 0){
-//					fileText += similar.get(j);
-//				}
-//				else {
-//					fileText += similar.get(j);
-//				}
-//				fileText += ",";
-//			}
-//
-//			fileText += "\n";
-//		}
-//
-//		// Print file text to file named by parameter outputFileName
-//		try {
-//			PrintWriter p = new PrintWriter(new File(outputFileName));
-//			p.print(fileText);
-//			p.close();
-//		}
-//		catch (IOException e){
-//			e.printStackTrace();
-//		}
-//
-//	}
 }
